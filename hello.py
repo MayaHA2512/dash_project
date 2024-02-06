db8bafb542084675d5787e8aa77f942d262e3ce4
mayahettiarachchige <maya.hetti-arachchig@ada.ac.uk>
2024-02-06 11:38:35 +0000

    quatro commit

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
-print('hello world')
+import sqlite3
+
+
+conn = sqlite3.connect('dash.db')
+c = conn.cursor()
+
+# Create table if it doesn't exist
+c.execute('''
+    CREATE TABLE IF NOT EXISTS wallet (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        balance INTEGER
+    )
+''')
+
+# Initialize 'students' row if it doesn't exist
+c.execute('INSERT OR IGNORE INTO wallet (id, balance) VALUES (1, 0)')
+
+conn.commit()
+conn.close()
