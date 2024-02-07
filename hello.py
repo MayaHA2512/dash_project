af89849c1b1e2d895af381f2cb558b6332e787db
mayahettiarachchige <maya.hetti-arachchig@ada.ac.uk>
2024-02-07 10:47:30 +0000

    Adding spending pie chart and making some changes to organisation of view file

--- hello.py
+++ hello.py
@@ -1,57 +1,71 @@
-import sqlite3
+# import sqlite3
+# #
+# #
+# conn = sqlite3.connect('dash.db')
+# c = conn.cursor()
+# #
+# # # Create table if it doesn't exist
+# c.execute('''
+#     CREATE TABLE IF NOT EXISTS transactions_tbl (
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         description TEXT,
+#         category TEXT,
+#         amount INTEGER,
+#         method TEXT,
+#         type TEXT
+#     )
+# ''')
 #
+# # # Initialize 'students' row if it doesn't exist
+# # c.execute("INSERT OR IGNORE INTO transactions_tbl (description, category, amount, method, type) VALUES ('Shopping Spree', 'leisure',  30, 'card', 'IN')")
+# #
+# # "INSERT OR IGNORE INTO transactions_tbl (description, category, amount, method, type) VALUES ('Shopping Spree', 'leisure',  30, 'card', 'IN')"
+# # conn.commit()
+# # conn.close()
 #
-conn = sqlite3.connect('dash.db')
-c = conn.cursor()
+# #
+# # # Fetch all rows from the query
+# # c.execute('DELETE FROM transactions WHERE id = 17')
+# # rows = c.fetchall()
+# #
+# # # Iterate over the rows and print them
+# # for row in rows:
+# #     print(row)
+# #
+# # # Close the cursor and the connection
+# # c.close()
+# # conn.close()
 #
-# # Create table if it doesn't exist
-c.execute('''
-    CREATE TABLE IF NOT EXISTS transactions_tbl (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        description TEXT,
-        category TEXT,
-        amount INTEGER,
-        method TEXT,
-        type TEXT
-    )
-''')
+# import sqlite3
+#
+# # Connect to the database (or create it if it doesn't exist)
+# conn = sqlite3.connect('example.db')
+#
+# # Create a cursor object
+# cur = conn.cursor()
+#
+# # SQL command to add a new column
+# # Replace 'table_name' with your table's name
+# # Replace 'new_column_name' with your new column's name
+# # Replace 'column_type' with your new column's type (e.g., TEXT, INTEGER, etc.)
+# c.execute('ALTER TABLE transactions_tbl ADD COLUMN date TEXT')
+#
+# # Commit the changes
+# conn.commit()
+#
+# # Close the connection
+# RESET THE DISPLAYED BALANCE
 
-# # Initialize 'students' row if it doesn't exist
-# c.execute("INSERT OR IGNORE INTO transactions_tbl (description, category, amount, method, type) VALUES ('Shopping Spree', 'leisure',  30, 'card', 'IN')")
+# import sqlite3
 #
-# "INSERT OR IGNORE INTO transactions_tbl (description, category, amount, method, type) VALUES ('Shopping Spree', 'leisure',  30, 'card', 'IN')"
+# conn = sqlite3.connect('dash.db')
+# c = conn.cursor()
+# c.execute('UPDATE wallet SET balance =  ? WHERE id = 1', (327.50,))
 # conn.commit()
 # conn.close()
 
-#
-# # Fetch all rows from the query
-# c.execute('DELETE FROM transactions WHERE id = 17')
-# rows = c.fetchall()
-#
-# # Iterate over the rows and print them
-# for row in rows:
-#     print(row)
-#
-# # Close the cursor and the connection
-# c.close()
-# conn.close()
-
-import sqlite3
-
-# Connect to the database (or create it if it doesn't exist)
-conn = sqlite3.connect('example.db')
-
-# Create a cursor object
-cur = conn.cursor()
-
-# SQL command to add a new column
-# Replace 'table_name' with your table's name
-# Replace 'new_column_name' with your new column's name
-# Replace 'column_type' with your new column's type (e.g., TEXT, INTEGER, etc.)
-c.execute('ALTER TABLE transactions_tbl ADD COLUMN date TEXT')
-
-# Commit the changes
-conn.commit()
-
-# Close the connection
-conn.close()
+import plotly.express as px
+# This dataframe has 244 lines, but 4 distinct values for `day`
+df = px.data.tips()
+fig = px.pie(df, values='tip', names='day')
+fig.show()
--- view.py
+++ view.py
@@ -3,69 +3,115 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 from model import model
 import dash_ag_grid as dag
+import plotly.express as px
 
 model_cfg = model()
 
 g = 1
-card_content = [
-    dbc.CardHeader("Add transaction"),
-    dbc.CardBody(
-        [
-            html.Div([
-                dbc.Input(id='description-box', type='text', style={'margin-bottom': '10px'}),
-                dbc.Input(id='input-box', type='text', style={'margin-bottom': '10px'}),
-                dbc.Select(
-                    id="select",
-                    options=[
-                        {"label": "Cash", "value": "cash"},
-                        {"label": "Credit Card", "value": "credit"},
-                        {"label": "Debit Card", "value": "debit"},
-                    ], style={'margin-bottom': '7px'}
-                ),
-dbc.Select(
-                    id="category",
-                    options=[
-                        {"label": "Bills", "value": "bills"},
-                        {"label": "Tuition fees", "value": "tuition fees"},
-                        {"label": "Books", "value": "books"},
-                    ], style={'margin-bottom': '7px'}
-                ),
-                dbc.Button('Save', id='save-button', style={'margin-bottom': '7px', 'margin-right': '4px'},
-                           color='secondary'),
-                dbc.Button('Spend', id='spend-button', style={'margin-bottom': '7px'}, color='secondary'),
-            ], ),
-        ], className="d-flex justify-content-center align-items-center"
-    ),
-]
-card = dbc.Card(card_content, color="dark", inverse=True)
+# card_content = [
+#     dbc.CardHeader("Add transaction"),
+#     dbc.CardBody(
+#         [
+#             html.Div([
+#                 dbc.Input(id='description-box', type='text', style={'margin-bottom': '10px'}),
+#                 dbc.Input(id='input-box', type='text', style={'margin-bottom': '10px'}),
+#                 dbc.Select(
+#                     id="select",
+#                     options=[
+#                         {"label": "Cash", "value": "cash"},
+#                         {"label": "Credit Card", "value": "credit"},
+#                         {"label": "Debit Card", "value": "debit"},
+#                     ], style={'margin-bottom': '7px'}
+#                 ),
+# dbc.Select(
+#                     id="category",
+#                     options=[
+#                         {"label": "Bills", "value": "bills"},
+#                         {"label": "Tuition fees", "value": "tuition fees"},
+#                         {"label": "Books", "value": "books"},
+#                     ], style={'margin-bottom': '7px'}
+#                 ),
+#                 dbc.Button('Save', id='save-button', style={'margin-bottom': '7px', 'margin-right': '4px'},
+#                            color='secondary'),
+#                 dbc.Button('Spend', id='spend-button', style={'margin-bottom': '7px'}, color='secondary'),
+#             ], ),
+#         ], className="d-flex justify-content-center align-items-center"
+#     ),
+# ]
+# card = dbc.Card(card_content, color="dark", inverse=True)
+
+df = model_cfg.get_data_df()
+pie_chart = px.pie(df, values='amount', names='category')
+pie_chart = pie_chart.update_layout(paper_bgcolor='rgba(0,0,0,0)', )
 
 
 class FinanceView:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def card():
+        card_content = [
+            dbc.CardHeader("Add transaction"),
+            dbc.CardBody(
+                [
+                    html.Div([
+                        dbc.Input(id='description-box', type='text', style={'margin-bottom': '10px'}),
+                        dbc.Input(id='input-box', type='text', style={'margin-bottom': '10px'}),
+                        dbc.Select(
+                            id="select",
+                            options=[
+                                {"label": "Cash", "value": "cash"},
+                                {"label": "Credit Card", "value": "credit"},
+                                {"label": "Debit Card", "value": "debit"},
+                            ], style={'margin-bottom': '7px'}
+                        ),
+                        dbc.Select(
+                            id="category",
+                            options=[
+                                {"label": "Bills", "value": "bills"},
+                                {"label": "Tuition fees", "value": "tuition fees"},
+                                {"label": "Books", "value": "books"},
+                            ], style={'margin-bottom': '7px'}
+                        ),
+                        dbc.Button('Save', id='save-button', style={'margin-bottom': '7px', 'margin-right': '4px'},
+                                   color='secondary'),
+                        dbc.Button('Spend', id='spend-button', style={'margin-bottom': '7px'}, color='secondary'),
+                    ], ),
+                ], className="d-flex justify-content-center align-items-center"
+            ),
+        ]
+        card = dbc.Card(card_content, color="dark", inverse=True)
+        return card
 
     @staticmethod
     def login_layout():
         return html.Div([
             html.H4('Please Log In', style={'padding': '15px'}),
-            dcc.Input(id='username-input', placeholder='Enter your username', type='text',
+            dbc.Input(id='username-input', placeholder='Enter your username', type='text',
                       style={'margin-left': '15px', 'margin-bottom': '10px'}),
             html.Br(),
-            dcc.Input(id='password-input', placeholder='Enter your password', type='password',
+            dbc.Input(id='password-input', placeholder='Enter your password', type='password',
                       style={'margin-left': '15px', 'margin-bottom': '10px'}),
             html.Br(),
-            html.Button('Login', id='login-button', style={'margin-left': '15px', 'margin-bottom': '10px'}),
+            dbc.Button('Login', id='login-button', style={'margin-left': '15px', 'margin-bottom': '10px'},
+                       color='secondary'),
             html.Div(id='login-output', style={'margin-left': '15px'})
         ])
 
     def success_layout(self):
+        card = FinanceView.card()
         return html.Div([dbc.Row([
             html.Div([
                 html.H4('Welcome back!', style={'margin-left': '15px'}),
                 html.H4('Balance: £', style={'margin-left': '15px'}),
                 html.H4(model_cfg.get_balance(), id='balance', style={'margin-left': '0px'}),
-                html.Button('Logout', id='logout-button', style={'margin-left': '15px', 'margin-bottom': '10px'}),
+                dbc.Button('Logout', id='logout-button', style={'margin-left': '15px', 'margin-bottom': '10px'},
+                           color='secondary'),
             ], style={'display': 'flex'}),
             dbc.Row(
-                [dbc.Col(card, width=4, style={'margin-left': '15px'}),
+                [dbc.Col([card, dcc.Graph(id='pie_chart', figure=pie_chart, )], width=4,
+                         style={'margin-left': '15px'}),
                  dbc.Col(
                      dag.AgGrid(
                          id='transaction-table',
@@ -75,8 +121,9 @@ class FinanceView:
                      )
                  ),
                  ]),
+
         ]),
-        ])
+        ]),
 
     def navbar(self):
         navbar = dbc.NavbarSimple(
